5d0b61b76f7bba83d8225e033887f4af1022d1b3
Alisue <lambdalisue@gmail.com>
2025-12-16 22:49:12 +0900

    feat: add Nix flake for development and distribution
    - Add flake.nix with packages, apps, and devShells outputs
    - Enable `nix run github:jsr-probitas/cli` for direct execution
    - Enable `nix profile install github:jsr-probitas/cli` for installation
    - Enable `nix develop` for development environment

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Probitas CLI - Command-line interface for Probitas";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        probitas = pkgs.writeShellApplication {
+          name = "probitas";
+          runtimeInputs = [ pkgs.deno ];
+          text = ''
+            export DENO_NO_UPDATE_CHECK=1
+            exec deno run -A \
+              --config=${self}/deno.json \
+              --lock=${self}/deno.lock \
+              ${self}/mod.ts "$@"
+          '';
+        };
+      in
+      {
+        packages = {
+          inherit probitas;
+          default = probitas;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = probitas;
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            deno
+          ];
+
+          shellHook = ''
+            echo "Entering Probitas CLI development environment"
+          '';
+        };
+      }
+    );
+}
